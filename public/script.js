23b50414d56072738442da1d8eb34b7e8c3daf91
0xasif.eth <52921890+rifatmax67@users.noreply.github.com>
2025-07-04 02:24:24 +0600

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,1 +1,20 @@
+const lb = document.getElementById('leaderboard');
 
+fetch('/api/leaderboard')
+  .then(res => res.json())
+  .then(({ data }) => {
+    lb.innerHTML = '';
+    data.forEach((kol, i) => {
+      const div = document.createElement('div');
+      div.className = 'kol';
+      div.innerHTML = `
+        <span>#${i + 1} – ${kol.author_name}</span>
+        <span class="score">${kol.score.toFixed(2)}</span>
+      `;
+      lb.appendChild(div);
+    });
+  })
+  .catch(err => {
+    lb.innerHTML = '<p>Failed to load leaderboard.</p>';
+    console.error(err);
+  });
